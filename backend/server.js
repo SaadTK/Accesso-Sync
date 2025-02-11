1e224d19204c7cfef5dfc556f001d083d66c3661
SaadTK <tahmid.karim.saad@gmail.com>
2025-02-11 16:14:10 +0600

    first commit

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+// import experss from "experess";
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5010;
+// console.log(process.env.PORT);
+
+//routes
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log("Server is running on http://localhost:" + PORT);
+  connectDB();
+});

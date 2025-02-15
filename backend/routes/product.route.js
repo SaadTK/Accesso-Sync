d352b268790eaf1a26319572ba38def87bab3a63
SaadTK <tahmid.karim.saad@gmail.com>
2025-02-15 16:50:52 +0600

    fixed createProduct

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -10,6 +10,6 @@ const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
 router.get("/featured", getFeaturedProducts);
-router.post("/", protectRoute, adminRoute createProduct);
+router.post("/", protectRoute, adminRoute, createProduct);
 
 export default router;

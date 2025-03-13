bbe5db6b4b95aadb6ced59be8476479113f1ccd3
SaadTK <tahmid.karim.saad@gmail.com>
2025-03-13 21:24:45 +0600

    Login page frontend done

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { UserPlus, Mail, Lock, User, ArrowRight, Loader } from "lucide-react";
+import { UserPlus,  User, ArrowRight, Loader } from "lucide-react";
 import { motion } from "framer-motion";
 
 const SignUpPage = () => {
